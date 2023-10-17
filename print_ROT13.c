2b22dd67ae09cc28db4227088ca3babeba34e56f
Eunicekahinga <euniwahito@gmail.com>
2023-10-17 19:42:50 +0300

    Rot13 file

---
+++ print_ROT13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_rot13 - encodes a string in ROT-13
+ * @a: the string being encoded
+ * Return: a pointer to the encoded string
+ */
+int print_rot13(char *a)
+{
+	int i;
+	int j;
+	int rot_len;
+	char *def = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	rot_len = 0;
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[i] == def[j])
+			{
+				a[i] = rot[j];
+				break;
+			}
+		}
+	}
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		_putchar(a[i]);
+		rot_len++;
+	}
+	return (rot_len);
+}
