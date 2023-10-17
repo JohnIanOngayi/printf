83d6c50d102e31175c4db9cb1c2872f3e8a95b36
Eunicekahinga <euniwahito@gmail.com>
2023-10-17 19:39:11 +0300

    printf file

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * _printf - emulates printf
+ * @format: string containing format specifiers
+ * Return: 0, success -1 for fail
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count;
+	bool error;
+	int result;
+
+	va_start(args, format);
+	count = 0;
+	error = false;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+			{
+				error = true;
+				break;
+			}
+			result = spec(*format, args);
+			if (result == -1)
+			{
+				error = true;
+				break;
+			}
+			count += result;
+		}
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	va_end(args);
+	if (error)
+		return (-1);
+	return (count);
+}
